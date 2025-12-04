99dde21983ad91761a2300e0eba06783ee154ebc
Mohit sharma <mohitsharma96185@gmail.com>
2025-12-04 20:05:10 +0530

    changed headless to true

--- lib/workflow/executor/LaunchBrowserExecutor.ts
+++ lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -10,7 +10,7 @@ export async function LaunchBrowserExecutor(environment:ExecutionEnvironment<typ
    try {
     const websiteUrl=environment.getInput("website Url");
     const browser=await puppeteer.launch({
-        headless:false,
+        headless:true,
     })
      environment.log.info("Browser started successfully")
     environment.setBrowser(browser);
