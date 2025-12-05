bf910608729d63400398639bebb0f21b5870f49e
Mohit sharma <mohitsharma96185@gmail.com>
2025-12-05 08:18:37 +0530

    resolved issues related to launchbrowser

--- lib/workflow/executor/LaunchBrowserExecutor.ts
+++ lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -1,33 +1,74 @@
+// import { waitFor } from "@/lib/helper/waitFor"
+// import { Environment, ExecutionEnvironment } from "@/types/executor";
+// import puppeteer from "puppeteer"
+// import { LaunchBrowserTask } from "../task/LaunchBrowser";
+
+
+
+// export async function LaunchBrowserExecutor(environment:ExecutionEnvironment<typeof LaunchBrowserTask>):Promise<boolean>{
+    
+//    try {
+//     const websiteUrl=environment.getInput("website Url");
+//     const browser=await puppeteer.launch({
+//         headless:true,
+//     })
+//      environment.log.info("Browser started successfully")
+//     environment.setBrowser(browser);
+//     const page=await browser.newPage();
+
+
+//     await page.goto(websiteUrl);
+//     await waitFor(2000);
+//     environment.setPage(page);
+
+//      environment.log.info(`Opened page at ${websiteUrl}`)
+//     return true;
+//    } catch (error:any) {
+//     environment.log.error(error.message);
+//     return false;
+//    }
+// }
+
+
+
 import { waitFor } from "@/lib/helper/waitFor"
 import { Environment, ExecutionEnvironment } from "@/types/executor";
 import puppeteer from "puppeteer"
 import { LaunchBrowserTask } from "../task/LaunchBrowser";
 
+export async function LaunchBrowserExecutor(environment: ExecutionEnvironment<typeof LaunchBrowserTask>): Promise<boolean> {
+  try {
+    const websiteUrl = environment.getInput("website Url");
 
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+      ],
+      executablePath: puppeteer.executablePath(), // important for Railway
+    });
 
-export async function LaunchBrowserExecutor(environment:ExecutionEnvironment<typeof LaunchBrowserTask>):Promise<boolean>{
-    
-   try {
-    const websiteUrl=environment.getInput("website Url");
-    const browser=await puppeteer.launch({
-        headless:true,
-    })
-     environment.log.info("Browser started successfully")
+    environment.log.info("Browser started successfully");
+
     environment.setBrowser(browser);
-    const page=await browser.newPage();
-
-
+    const page = await browser.newPage();
     await page.goto(websiteUrl);
     await waitFor(2000);
-    environment.setPage(page);
 
-     environment.log.info(`Opened page at ${websiteUrl}`)
+    environment.setPage(page);
+    environment.log.info(`Opened page at ${websiteUrl}`);
     return true;
-   } catch (error:any) {
+
+  } catch (error: any) {
     environment.log.error(error.message);
     return false;
-   }
+  }
 }
+
+
 
 
 
